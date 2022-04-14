52676c159d26f24a50610dd84fdf0e9dc093f52b
Maha <mahariaz96@gmail.com>
2022-04-14 18:18:13 +0500

    Highlight Commit

--- app/src/main/java/com/mahariaz/smartsalah/monStats.java
+++ app/src/main/java/com/mahariaz/smartsalah/monStats.java
@@ -27,6 +27,7 @@ import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
 import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
 import com.github.mikephil.charting.formatter.ValueFormatter;
+import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
 import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.text.SimpleDateFormat;
@@ -38,7 +39,7 @@ import java.util.Locale;
 
 public class monStats extends Fragment {
 
-    BarChart barChart;
+    BarChart mChart;
     // variable for our bar data.
     BarData barData;
     // variable for our bar data set.
@@ -54,48 +55,89 @@ public class monStats extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        View view= inflater.inflate(R.layout.fragment_weekly_stats, container, false);
+        View view= inflater.inflate(R.layout.fragment_mon_stats, container, false);
 
 
 
-        barChart = view.findViewById(R.id.idBarChart);
-        barEntriesArrayList=new ArrayList<>();
-        labelNames=new ArrayList<>();
-        fillPostureList();
-        for (int i=0;i<PostureNamesArrayList.size();i++){
-            String postures=PostureNamesArrayList.get(i).getPostureName();
-            int avgPostureTime=PostureNamesArrayList.get(i).getAvgPostureTime();
-            barEntriesArrayList.add(new BarEntry(i,avgPostureTime));
-            labelNames.add(postures);
-        }
-        BarDataSet barDataSet=new BarDataSet(barEntriesArrayList,"Postures");
-        barDataSet.setColors(ColorTemplate.PASTEL_COLORS);
-        Description description=new Description();
-        description.setText("");
-        barChart.setDescription(description);
-        BarData barData=new BarData(barDataSet);
-        barChart.setData(barData);
-        XAxis xAxis=barChart.getXAxis();
-        YAxis rightYAxis = barChart.getAxisRight();
-        rightYAxis.setEnabled(false);
-        xAxis.setValueFormatter(new IndexAxisValueFormatter(labelNames));
+        mChart = (BarChart) view.findViewById(R.id.idBarChart);
+        mChart.setDrawBarShadow(false);
+        mChart.getDescription().setEnabled(false);
+        mChart.setPinchZoom(false);
+
+        // empty labels so that the names are spread evenly
+        String[] labels = {"", "Fajar", "Zuhr", "Asar", "Maghrib", "Isha", ""};
+        XAxis xAxis = mChart.getXAxis();
+        xAxis.setCenterAxisLabels(true);
         xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
         xAxis.setDrawGridLines(true);
-        xAxis.setDrawAxisLine(false);
-        xAxis.setDrawGridLinesBehindData(false);
-        xAxis.setGranularity(1f);
-        xAxis.setLabelCount(labelNames.size());
+        xAxis.setGranularity(1f); // only intervals of 1 day
+
+
+
+        xAxis.setAxisMinimum(1f);
+        xAxis.setValueFormatter(new IndexAxisValueFormatter(labels));
         xAxis.setLabelRotationAngle(360);
-        barChart.animateY(2000);
-        barChart.invalidate();
+        mChart.animateY(2000);
+        YAxis leftAxis = mChart.getAxisLeft();
+
+
+
+
+        leftAxis.setLabelCount(8, true);
+
+
+        mChart.getAxisRight().setEnabled(false);
+        mChart.getLegend().setEnabled(false);
+
+        float[] week1 = {10, 20, 30, 40, 50};
+        float[] week2 = {60, 50, 40, 30, 20};
+        float[] week3 = {50, 60, 20, 10, 30};
+        float[] week4 = {50, 60, 20, 10, 30};
+
+        ArrayList<BarEntry> barOne = new ArrayList<>();
+        ArrayList<BarEntry> barTwo = new ArrayList<>();
+        ArrayList<BarEntry> barThree = new ArrayList<>();
+        ArrayList<BarEntry> barFour = new ArrayList<>();
+        for (int i = 0; i < week1.length; i++) {
+            barOne.add(new BarEntry(i, week1[i]));
+            barTwo.add(new BarEntry(i, week2[i]));
+            barThree.add(new BarEntry(i, week3[i]));
+            barFour.add(new BarEntry(i, week4[i]));
+        }
+
+        BarDataSet set1 = new BarDataSet(barOne, "barOne");
+        set1.setColor(Color.parseColor("#FFA6C9"));
+        BarDataSet set2 = new BarDataSet(barTwo, "barTwo");
+        set2.setColor(Color.parseColor("#FAD6A5"));
+        BarDataSet set3 = new BarDataSet(barThree, "barTwo");
+        set3.setColor(Color.parseColor("#CD5B45"));
+        BarDataSet set4 = new BarDataSet(barFour, "barTwo");
+        set4.setColor(Color.parseColor("#98817B"));
+
+
+        ArrayList<IBarDataSet> dataSets = new ArrayList<IBarDataSet>();
+        dataSets.add(set1);
+        dataSets.add(set2);
+        dataSets.add(set3);
+        dataSets.add(set4);
+
+        BarData data = new BarData(dataSets);
+        float groupSpace = 0.2f;
+        float barSpace = 0f;
+        float barWidth = 0.2f;
+        // (barSpace + barWidth) * 2 + groupSpace = 1
+        data.setBarWidth(barWidth);
+        // so that the entire chart is shown when scrolled from right to left
+        xAxis.setAxisMaximum(labels.length - 1.1f);
+        mChart.setData(data);
+        mChart.setScaleEnabled(false);
+        mChart.setVisibleXRangeMaximum(6f);
+        mChart.groupBars(1f, groupSpace, barSpace);
+
+        mChart.invalidate();
+
+
         return view;
     }
-    private void fillPostureList(){
-        PostureNamesArrayList.clear();
-        PostureNamesArrayList.add(new SalahPostureNames("Fajar",10));
-        PostureNamesArrayList.add(new SalahPostureNames("Zuhr",12));
-        PostureNamesArrayList.add(new SalahPostureNames("Asar",5));
-        PostureNamesArrayList.add(new SalahPostureNames("Maghrib",7));
-        PostureNamesArrayList.add(new SalahPostureNames("Isha",20));
-    }
+
 }
